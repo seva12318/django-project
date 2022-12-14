0e53b547d67cb70d94a9285ae6adffbbbda196b2
ponchik009 <lolikonru@mail.ru>
2022-12-14 16:15:52 +0800

    crud операции над уроками починены

--- client/src/views/Teacher/Subject.vue
+++ client/src/views/Teacher/Subject.vue
@@ -1,115 +1,125 @@
 <template>
-    <div class="wrapper">
-        <Loader v-if="!Boolean(lessons) || !Boolean(subject) || isLoading"/>
-        <div class="subject_wrapper" v-else>
-            <div class="subject_name">
+  <div class="wrapper">
+    <Loader v-if="!Boolean(lessons) || !Boolean(subject) || isLoading" />
+    <div class="subject_wrapper" v-else>
+      <div class="subject_name">
         <span class="subject_title">{{
-                `${subject.name} (${subject.level})`
-            }}</span>
-                <span>
+          `${subject.name} (${subject.level})`
+        }}</span>
+        <span>
           {{ `Время: ${subject.time}` }}
         </span>
-            </div>
-            <div class="lessons_wrapper" v-if="lessons.length === 0">
-                <div class="subject_no_lessons">
-                    На данный момент у вас нет уроков по этому предмету
+      </div>
+      <div class="lessons_wrapper" v-if="lessons.length === 0">
+        <div class="subject_no_lessons">
+          На данный момент у вас нет уроков по этому предмету
+        </div>
+        <div class="btn btn-green" @click="openAddLessonModal()">
+          + Добавить урок
+        </div>
+      </div>
+      <div class="lessons_wrapper" v-else>
+        <div class="btn btn-green" @click="openAddLessonModal()">
+          + Добавить урок
+        </div>
+        <div class="subject_lessons">
+          <div class="subject_lesson" v-for="(lesson, index) in sortedLessons">
+            <div>
+              <div class="d-flex justify-content-between">
+                <span class="title">{{
+                  `Урок №${sortedLessons.length - index}`
+                }}</span>
+                <div
+                  @click="onRemoveClicked(lesson)"
+                  class="btn btn-sm btn-danger"
+                >
+                  Удалить урок
                 </div>
-                <div class="btn btn-green" @click="openAddLessonModal()">+ Добавить урок</div>
+              </div>
+              <hr />
+              <div>
+                <span class="title">Тема: </span>
+                <span>{{ lesson.topic }}</span>
+              </div>
+              <div>
+                <span class="title">Дата: </span>
+                <span>{{ formatDate(lesson.date) }}</span>
+              </div>
+              <hr />
+              <span class="title">Домашнее задание:</span>
+              <span class="homework">{{ `${lesson.homework}` }}</span>
+              <hr />
             </div>
-            <div class="lessons_wrapper" v-else>
-                <div class="btn btn-green" @click="openAddLessonModal()">+ Добавить урок</div>
-                <div class="subject_lessons">
-                    <div class="subject_lesson" v-for="(lesson, index) in sortedLessons">
-                        <div>
-                            <div class="d-flex justify-content-between">
-              <span class="title">{{
-                      `Урок №${sortedLessons.length - index}`
-                  }}</span>
-                                <div @click="onRemoveClicked(lesson)" class="btn btn-sm btn-danger">Удалить урок</div>
-                            </div>
-                            <hr/>
-                            <div>
-                                <span class="title">Тема: </span>
-                                <span>{{ lesson.topic }}</span>
-                            </div>
-                            <div>
-                                <span class="title">Дата: </span>
-                                <span>{{ formatDate(lesson.date) }}</span>
-                            </div>
-                            <hr/>
-                            <span class="title">Домашнее задание:</span>
-                            <span class="homework">{{ `${lesson.homework}` }}</span>
-                            <hr/>
-                        </div>
-                        <div class="btns">
-                            <div
-                                class="btn btn-green "
-                                @click="
+            <div class="btns">
+              <div
+                class="btn btn-green"
+                @click="
                   openEditLessonModal({
                     ...lesson,
                     number: sortedLessons.length - index,
                   })
                 "
-                            >
-                                Редактировать
-                            </div>
-                            <div class="btn btn-green " @click="navigateToLesson(lesson.id)">
-                                Открыть журнал
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              >
+                Редактировать
+              </div>
+              <div class="btn btn-green" @click="navigateToLesson(lesson.id)">
+                Открыть журнал
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    <ModalLesson
-        :showModal="showModal"
-        :onClose="onModalClose"
-        :onSubmit="isEdit ? onUpdateLesson : onAddLesson"
-        :isEdit="isEdit"
-        :lesson="currentLesson"
-    />
+  </div>
+  <ModalLesson
+    :showModal="showModal"
+    :onClose="onModalClose"
+    :onSubmit="onAddLesson"
+    :isEdit="isEdit"
+    :lesson="currentLesson"
+  />
 </template>
 <script setup>
-import {computed, onBeforeMount, ref} from "vue";
-import {storeToRefs} from "pinia";
-import {useTeacherStore} from "@/stores/teacherStore";
-import {formatDate} from "@/util/formatDate";
+import { computed, onBeforeMount, ref, reactive } from "vue";
+import { storeToRefs } from "pinia";
+import { useTeacherStore } from "@/stores/teacherStore";
+import { formatDate } from "@/util/formatDate";
 import Loader from "../../components/Loader/Loader.vue";
 import ModalLesson from "./ModalAddLesson/ModalLesson.vue";
-import {useAuthStore} from "@/stores/authStore";
-import {useRoute, useRouter} from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+import { useRoute, useRouter } from "vue-router";
 
 const authStore = useAuthStore();
 const route = useRoute();
-const router = useRouter()
+const router = useRouter();
 const teacherStore = useTeacherStore();
 
 // добавить уроки и удалить из data, когда появится ручка
-const {subject, lessons, isLoading} = storeToRefs(teacherStore);
+const { subject, lessons, isLoading } = storeToRefs(teacherStore);
 const sortedLessons = computed(() => {
-    return lessons !== null
-        ? // descending sort
-        lessons.value.sort((l1, l2) => (l1.date > l2.date ? -1 : 1))
-        : null;
+  return lessons !== null
+    ? // descending sort
+      lessons.value.sort((l1, l2) => (l1.date > l2.date ? -1 : 1))
+    : null;
 });
 
-const subjectId = ref(route.params.id);
-const showModal = ref(false);
-const isEdit = ref(false);
-const currentLesson = ref({
-    topic: "",
-    homework: "",
-    date: new Date(Date.now()).toISOString().split("T")[0],
+let subjectId = ref(route.params.id);
+let showModal = ref(false);
+let isEdit = ref(false);
+
+let currentLesson = reactive({
+  topic: "",
+  homework: "",
+  date: new Date(Date.now()).toISOString().split("T")[0],
 });
 
 function onRemoveClicked(lesson) {
-    teacherStore.removeLesson(lesson);
+  teacherStore.removeLesson(lesson);
 }
 
 onBeforeMount(() => {
-    teacherStore.fetchSubjectLessons(route.params.id);
-    teacherStore.fetchSubjectById(route.params.id);
+  teacherStore.fetchSubjectLessons(route.params.id);
+  teacherStore.fetchSubjectById(route.params.id);
 });
 
 function navigateToLesson(lessonId) {
@@ -127,151 +137,148 @@ function onModalClose() {
 }
 
 function onAddLesson(lesson) {
-  teacherStore.addLesson({
-    ...lesson,
-    subjectId: Number(subjectId.value),
-    date: lesson.date,
-
-  });
-  onModalClose();
-}
-
-function onUpdateLesson(lesson) {
-  teacherStore.updateLesson(lesson.id, {
-    ...lesson,
-    date: lesson.date,
-  });
+  console.log(isEdit);
+  if (isEdit.value) {
+    teacherStore.updateLesson(lesson.id, {
+      ...lesson,
+      date: lesson.date,
+    });
+  } else {
+    teacherStore.addLesson({
+      ...lesson,
+      subjectId: Number(subjectId.value),
+      date: lesson.date,
+    });
+  }
   onModalClose();
 }
 
 function openAddLessonModal() {
   isEdit.value = false;
-  currentLesson.value = {
+  Object.assign(currentLesson, {
     topic: "",
     homework: "",
     date: new Date(Date.now()).toISOString().split("T")[0],
     //csrf: this.csrf
-  };
+  });
   onModalOpen();
 }
 
 function openEditLessonModal(lesson) {
+  console.log(lesson);
   isEdit.value = true;
-  currentLesson.value = lesson;
+  Object.assign(currentLesson, lesson);
   onModalOpen();
 }
 </script>
 
 <style scoped>
 .subject_wrapper {
-    display: flex;
-    flex-direction:column;
-    justify-content: flex-start;
-    flex: 1;
-    width: 90%;
-    padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex: 1;
+  width: 90%;
+  padding-top: 20px;
 }
 
 .subject_name {
-    display: flex;
-    flex-direction: column;
-    font-size: 24px;
-    font-weight: 600;
-    text-align: center;
-    color: rgb(5, 33, 84);
+  display: flex;
+  flex-direction: column;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  color: rgb(5, 33, 84);
 }
 
 .lessons_wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 20px;
-    gap: 20px;
-    color: rgb(5, 33, 84);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+  gap: 20px;
+  color: rgb(5, 33, 84);
 }
 
 .btn-green {
-    padding: 10px 20px;
-    border-radius: 8px;
-    background: rgb(5, 33, 84);
+  padding: 10px 20px;
+  border-radius: 8px;
+  background: rgb(5, 33, 84);
 
-    cursor: pointer;
+  cursor: pointer;
 
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-color:white;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: white;
+  text-align: center;
 
-    width: fit-content;
-    transition: all .3s;
+  width: fit-content;
+  transition: all 0.3s;
 }
 
 .btn-green:hover {
-    /*opacity: 0.9;*/
-    color: white;
-    background: rgb(5, 33, 84);
-    transform: scale(1.1);
+  /*opacity: 0.9;*/
+  color: white;
+  background: rgb(5, 33, 84);
+  transform: scale(1.1);
 }
 
 .subject_lessons {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 10px;
-    max-width: 100%;
-    padding-bottom: 100px;
-    color: rgb(5, 33, 84);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  max-width: 100%;
+  padding-bottom: 100px;
+  color: rgb(5, 33, 84);
 }
 
 .subject_lesson {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 20px 40px;
-    border-radius: 6px;
-    border: 1px solid gray;
-    min-width: 300px;
-    max-width: calc(100% / 4);
-    color: rgb(5, 33, 84);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 40px;
+  border-radius: 6px;
+  border: 1px solid gray;
+  min-width: 300px;
+  max-width: calc(100% / 4);
+  color: rgb(5, 33, 84);
 }
 
 .subject_lesson hr {
-    margin-top: 10px;
-    margin-bottom: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 
 .subject_lesson .title {
-    font-size: px;
-    font-weight: 500;
-    color: rgb(5, 33, 84);
+  font-size: px;
+  font-weight: 500;
+  color: rgb(5, 33, 84);
 }
 
 .subject_lesson .btns {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
-    align-items: center;
+  align-items: center;
 }
 
 .subject_no_lessons {
-    margin-top: 200px;
-    text-align: center;
-    color: rgb(5, 33, 84);
+  margin-top: 200px;
+  text-align: center;
+  color: rgb(5, 33, 84);
 }
 
 .homework {
-    width: 100px;
-    word-wrap: break-word;
-    color: rgb(5, 33, 84);
+  width: 100px;
+  word-wrap: break-word;
+  color: rgb(5, 33, 84);
 }
 
-.btn-sm btn-danger{
-    background-color: red;
+.btn-sm btn-danger {
+  background-color: red;
 }
-
-
-
 </style>
