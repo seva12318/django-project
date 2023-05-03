f79aa25494504bedfa4bbee3ec7286c929547431
Tommy <tomasmurlychenko@mail.ru>
2023-05-03 21:46:56 +0800

    Убрал debug_toolbar

--- lessons/settings.py
+++ lessons/settings.py
@@ -43,13 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "debug_toolbar",
     'marks',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
